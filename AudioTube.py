a9b0f9f03d441f2e9a53a944d9ffe80ca85a15ea
anmolp2010@gmail.com <anmolp2010@gmail.com>
2017-03-08 19:06:46 +0530

    Added Yes and No Message

--- AudioTube.py
+++ AudioTube.py
@@ -1,10 +1,25 @@
-import requests
 import json
 import pafy
+import shutil
 from urllib2 import urlopen
 
 API_KEY="AIzaSyDR4qtKowtX9gAbDMrg4C5suHKRe2L3fj4"
 BASE_URL="https://www.googleapis.com/youtube/v3/search?"
+class Audio:
+    def __init__(self,id):
+        self.id=id
+    def getID(self):
+        return self.id
+    def populate(self,audio,duration):
+        self.title=audio[3].title
+        self.duration=duration
+        self.size=audio[3].get_filesize()
+        self.type=audio[3].extension
+    def show(self):
+        print "Title:",self.title
+        print "Duration:",self.duration
+        print "Size:",self.size/float(1024*1024)
+        print "Type:",self.type
 def createAPIURL(filters):
     url=BASE_URL
     ct=0
@@ -16,31 +31,49 @@ def createAPIURL(filters):
         url = url+"&" + filter[0]+"="+ filter[1]
     return url
 
-def getVideoID(responses):
-    for response in responses:
-        return response["id"]["videoId"]
-
+def getAudio(response):
+    id=response["id"]["videoId"]
+    aud=Audio(id)
+    return aud
 
 typ="video"
 maxResults=str(5)
-query="\"cheap thrills\""
+query=raw_input("")
 part="snippet"
 filter = [("type",typ),("maxResults",maxResults),("q",query),("key",API_KEY),("part",part)]
 req=createAPIURL(filter)
-gt=requests.get(req)
-x=json.loads(gt.text)
-videoID=getVideoID(x["items"])
-video=pafy.new(videoID)
-audio=video.audiostreams
-u = urlopen(audio[3].url)
-inf=u.info()
-print inf.subtype
-data=u.read(1000000)
-with open('./test.mp3','wb') as output:
-  output.write(data)
-  """
+print req
+gt=None
 ct=0
-while(data):
-    print data
-    ct+=1
-    data=u.read(1000)"""
+while 1:
+    try:
+        #gt=requests.get(req)
+        gt=urlopen(req).read()
+        break
+    except:
+        ct+=1
+        print "try",ct
+        if ct>5:
+            print "Some Other time"
+            exit(1)
+        continue
+x=json.loads(gt)
+downloadFile=None
+for item in x["items"]:
+    audioData=getAudio(item)
+    video=pafy.new(audioData.getID())
+    audio=video.audiostreams
+    audioData.populate(audio,video.duration)
+    audioData.show()
+    print "Want to download(y/n)"
+    yn=raw_input("")
+    if yn=="y":
+        downloadFile=audio[3]
+        break
+try:
+    print "Downloading"
+    downloadFile.download(".\Songs")
+    print "Downloaded"
+except WindowsError:
+    #shutil.rmtree(".\Songs\\"+audio[-1].title+"."+audio[-1].extension+".temp")
+    print "Failed"
